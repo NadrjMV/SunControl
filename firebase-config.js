364c884b2a0d769ebdc0bee06ad0cf177443e268
NadrjMV <jordanmvpower@gmail.com>
2025-07-29 08:29:40 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0UFqsNRUGVwDpYcwduab8WaxoVrMxiBA",
+    authDomain: "suncontrol-d2d94.firebaseapp.com",
+    projectId: "suncontrol-d2d94",
+    storageBucket: "suncontrol-d2d94.appspot.com",
+    messagingSenderId: "434845455719",
+    appId: "1:434845455719:web:06cb665d1274c973f4cbc2"
+};
