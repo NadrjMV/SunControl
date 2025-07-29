c33b29a9171335f9a8ed59270ab1e2525b823b3d
NadrjMV <jordanmvpower@gmail.com>
2025-07-29 12:38:08 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,11 @@
-export const firebaseConfig = {
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// Configuração principal do Firebase (do seu projeto SunControl)
+const firebaseConfig = {
     apiKey: "AIzaSyA0UFqsNRUGVwDpYcwduab8WaxoVrMxiBA",
     authDomain: "suncontrol-d2d94.firebaseapp.com",
     projectId: "suncontrol-d2d94",
@@ -6,3 +13,8 @@ export const firebaseConfig = {
     messagingSenderId: "434845455719",
     appId: "1:434845455719:web:06cb665d1274c973f4cbc2"
 };
+
+// Inicializa e exporta os serviços do Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
